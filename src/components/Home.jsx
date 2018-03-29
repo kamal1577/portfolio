5f8ac12cd1bdebe4f15e858ee615c4a222e45605
kamalelazizialaoui <elazizialaouikamal@yahoo.com>
2018-03-29 00:43:50 -0400

    modification

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, col, Image, Button } from 'react-bootstrap';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css';
 
 export default class Home extends Component {
   render() {
     return(
       <Grid>
-        <Jumbotron>
-          <h2>Welcome to kamal's Portfolio</h2>
-          <p>This is Where to Know more about me!</p>
-          <Link to="/about">
-            <Button bsStyle="primary">About</Button>
-
-          </Link>
-
-
-        </Jumbotron>
-      </Grid>
+            <Jumbotron>
+              <h2>Welcome to kamal's Portfolio</h2>
+              <p>This is Where to Know more about me!</p>
+              <Link to="/about">
+                <Button bsStyle="primary">About</Button>
+              </Link>
+            </Jumbotron>
+            <Row className="show-grid text-center">
+              <Col xs={12} sm={4} className="person-wrapper">
+                <Image src="assets/person-3.jpg" circle className="profile-pic" />
+                <h3>Kamal Azizi Alaoui</h3>
+                <p> This is hhh chchhhb</p>
+              </Col>
+            </Row>
+       </Grid>
 
     )
   }
